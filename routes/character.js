b6fc94fcea951bb6811412f280c6be242615cfe2
carlyleec <carlyleec@gmail.com>
2017-05-10 00:55:12 -0600

    basic implementation of the single character, need refining

---
+++ routes/character.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const fromStarWars = require('../utils/fromStarWars');
+
+router.get('/', async (req, res) => {
+  const data = {
+    name: '',
+    characters: [],
+  };
+
+  res.render('character', data);
+});
+
+router.post('/', async (req, res) => {
+  res.redirect(`character/${req.body.name}`);
+});
+
+router.get('/:name', async (req, res) => {
+  const character = await fromStarWars(`/api/people/?search=${req.params.name}`);
+
+  const data = {
+    name: req.params.name,
+    characters: character.results,
+  };
+
+  res.render('character', data);
+});
+
+module.exports = router;
